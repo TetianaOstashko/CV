458f52951a7576e22f887d7a88ebc676f91a8e3a
TetianaOstashko <tetianaostashko.25@gmail.com>
2023-04-22 18:45:06 +0200

    Code for lesson 34

--- projects.js
+++ projects.js
@@ -1,25 +1,45 @@
 window.addEventListener('load', function() {
-    fetch('https://api.github.com/users/TetianaOstashko/repos')
-      .then(response => response.json())
-      .then(data => {
-        const projectList = document.getElementById('project-list');
-        data.forEach(repo => {
-          const li = document.createElement('li');
-          const a = document.createElement('a');
-          a.textContent = repo.full_name;
-          a.href = repo.html_url;
-          li.appendChild(a);
-          if (repo.description) {
-            const p = document.createElement('p');
-            p.textContent = repo.description;
-            li.appendChild(p);
-          }
-          projectList.appendChild(li);
-        });
+  class GithubApi {
+    constructor(username, token) {
+      this.username = username;
+      this.token = token;
+    }
+  
+    async getRepos() {
+      const response = await fetch(`https://api.github.com/users/${this.username}/repos`, {
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+        },
+      });
+      const data = await response.json();
+      return data;
+    }
+  }
+  
+  const githubApi = new GithubApi('TetianaOstashko', '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');
+  
+  const reposList = document.getElementById('project-list');
+  
+  githubApi.getRepos()
+    .then(data => {
+      for (let repo of data) {
+        const repoItem = document.createElement('li');
+        const repoLink = document.createElement('a');
+        repoLink.href = repo.html_url;
+        repoLink.textContent = repo.full_name;
+        repoItem.appendChild(repoLink);
+        if (repo.description) {
+          const repoDesc = document.createElement('p');
+          repoDesc.textContent = repo.description;
+          repoItem.appendChild(repoDesc);
+        }
+        reposList.appendChild(repoItem);
+      }
     })
     .catch(error => {
-        console.error('Помилка завантаження даних про репозиторії з GitHub', error);
+      console.error(error);
     });
 });
+
 
 
