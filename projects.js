bbd4f88fbab411387be54efa11a625c5c77b2003
TetianaOstashko <tetianaostashko.25@gmail.com>
2023-04-30 22:10:53 +0200

    Code without token. Token needs to be written manually while checking code for lesson 34

--- projects.js
+++ projects.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function() {
+/*window.addEventListener('load', function() {
   class GithubApi {
     constructor(username, token) {
       this.username = username;
@@ -41,5 +41,50 @@ window.addEventListener('load', function() {
     });
 });
 
+*/
+
+window.addEventListener('load', function() {
+  class GithubApi {
+    constructor(username, token) {
+      this.username = username;
+      this.token = token;
+    }
+  
+    async getRepos() {
+      const response = await fetch(`https://api.github.com/users/${this.username}/repos`, {
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+        },
+      });
+      const data = await response.json();
+      return data;
+    }
+  }
+  
+  const githubApi = new GithubApi('TetianaOstashko', 'insert token');
+  
+  const reposList = document.getElementById('repos-list');
+  
+  githubApi.getRepos()
+    .then(data => {
+      const dataArray = Object.values(data);
+      dataArray.forEach(repo => {
+        const repoItem = document.createElement('li');
+        const repoLink = document.createElement('a');
+        repoLink.href = repo.html_url;
+        repoLink.textContent = repo.full_name;
+        repoItem.appendChild(repoLink);
+        if (repo.description) {
+          const repoDesc = document.createElement('p');
+          repoDesc.textContent = repo.description;
+          repoItem.appendChild(repoDesc);
+        }
+        reposList.appendChild(repoItem);
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
 
 
